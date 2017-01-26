821d76364058bb08b67d087a7834a35efa269a73
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-26 16:20:38 +0100

    add full suite of tests

--- rover.test.php
+++ rover.test.php
@@ -15,7 +15,7 @@ class testRover extends TestCase{
 			);
 	}
 
-	public function testTurnToRight(){
+	public function testTurnToRightWhenFacingNorth(){
 		$rover3= new Rover();
 		$rover3->executeCommands(["r"]);
 		$this->assertEquals(
@@ -27,7 +27,7 @@ class testRover extends TestCase{
 				)
 			);
 	}
-	
+
 	public function testMoveForwardWhenFacingNorth(){
 		$rover2 = new Rover();
 		$rover2->executeCommands(["f"]);
@@ -129,6 +129,94 @@ class testRover extends TestCase{
 				"x"=>1,
 				"y"=>0,
 				"direction"=>"west"
+				)
+			);
+	}
+	public function testTurnRightWhenFacingEast(){
+		$rover = new Rover();
+		$rover->executeCommands(["r","r"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"south"
+				)
+			);
+	}
+
+	public function testTurnRightWhenFacingSouth(){
+		$rover = new Rover();
+		$rover->executeCommands(["r","r","r"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"west"
+				)
+			);
+	}
+
+	public function testTurnRightWhenFacingWest(){
+		$rover = new Rover();
+		$rover->executeCommands(["r","r","r","r"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"north"
+				)
+			);
+	}
+
+	public function testTurnLeftWhenFacingNorth(){
+		$rover = new Rover();
+		$rover->executeCommands(["l"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"west"
+				)
+			);
+	}
+
+	public function testTurnLeftWhenFacingWest(){
+		$rover = new Rover();
+		$rover->executeCommands(["l","l"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"south"
+				)
+			);
+	}
+	public function testTurnLeftWhenFacingSouth(){
+		$rover = new Rover();
+		$rover->executeCommands(["l","l","l"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"east"
+				)
+			);
+	}
+	public function testTurnLeftWhenFacingEast(){
+		$rover = new Rover();
+		$rover->executeCommands(["l","l","l","l"]);
+		$this->assertEquals(
+			$rover->getCoordinates(),
+			array(
+				"x"=>0,
+				"y"=>0,
+				"direction"=>"north"
 				)
 			);
 	}
