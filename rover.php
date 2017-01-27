4a00fdb790689362c8ddf81456daf3c69fb8388f
Mathias Ducancel <mathiasduc@gmail.com>
2017-01-27 10:49:01 +0100

    add a refactoring of code + new public commands

--- rover.php
+++ rover.php
@@ -10,9 +10,9 @@ class Rover {
 	public function getCoordinates(){
 		return $this->_coordinates;
 	}
+
 	public function executeCommands($arrayCommands){
 		$arrayCommandsLength= count($arrayCommands);
-
 		for ($i=0;$i < $arrayCommandsLength; $i++){
 			if($arrayCommands[$i] === "f" || $arrayCommands[$i] === "b"){
 				$this->moveRover($arrayCommands[$i]);
@@ -21,26 +21,40 @@ class Rover {
 			}
 		}
 	}
+
 	private function turnRover($command){
-		$currentDirection = $this->_coordinates["direction"]; 
-		$this->_coordinates["direction"] = $command === "r"? $currentDirection + 1 : 
-		$currentDirection -1;
-		if($this->_coordinates["direction"] === -1){
-			$this->_coordinates["direction"] = 3;
-		}elseif($this->_coordinates["direction"] === 4){
+		$command === "r"? $this->turnRight():$this->turnLeft();	
+	}
+
+	public function turnRight(){
+		$currentDirection = $this->_coordinates["direction"];
+		$this->_coordinates["direction"] = $currentDirection +1;
+		if($this->_coordinates["direction"] === 4){
 			$this->_coordinates["direction"] = 0;
 		}
 	}
 
-	private function moveRover($command){
+	public function turnLeft(){
 		$currentDirection = $this->_coordinates["direction"];
-		if($command === "f"){
-			$this->_coordinates["y"] += $this->_moveY[$currentDirection];
-			$this->_coordinates["x"] += $this->_moveX[$currentDirection];
-		}else{
-			$this->_coordinates["y"] -= $this->_moveY[$currentDirection];
-			$this->_coordinates["x"] -= $this->_moveX[$currentDirection];
+		$this->_coordinates["direction"] = $currentDirection -1;
+		if($this->_coordinates["direction"] === -1){
+			$this->_coordinates["direction"] = 3;
 		}
+	}
+
+	private function moveRover($command){
+		$command === "f"? $this->moveForward():$this->moveBackward();
+	}
+
+	public function moveForward(){
+		$currentDirection = $this->_coordinates["direction"];
+		$this->_coordinates["y"] += $this->_moveY[$currentDirection];
+		$this->_coordinates["x"] += $this->_moveX[$currentDirection];
+	}
+	public function moveBackward(){
+		$currentDirection = $this->_coordinates["direction"];
+		$this->_coordinates["y"] -= $this->_moveY[$currentDirection];
+		$this->_coordinates["x"] -= $this->_moveX[$currentDirection];
 	}
 }
 
